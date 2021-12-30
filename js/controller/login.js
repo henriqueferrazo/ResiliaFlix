7b70fbefcd6076b1571a3eaf19fcb769c52923c6
Rodrigo Cruz <rodrigonascimentocruz@gmail.com>
2021-12-30 19:21:29 -0300

    criei a validação das inscrições

--- js/controller/login.js
+++ js/controller/login.js
@@ -1,43 +1,39 @@
-const entrar = document.getElementById('btnEntrar');
-entrar.addEventListener('click', (event) => {
-    event.preventDefault()
-    
+const entrar = document.getElementById("btnEntrar");
+entrar.addEventListener("click", (event) => {
+  event.preventDefault();
 
-    const email = document.getElementById('email');
-    const senha = document.getElementById('senha');
+  const email = document.getElementById("email");
+  const senha = document.getElementById("senha");
 
-   
-        if(email.value == ''){
-           email.classList.add('errorInput');
-        } else {
-            email.classList.remove('errorInput');
-        }
+  if (email.value == "") {
+    email.classList.add("errorInput");
+  } else {
+    email.classList.remove("errorInput");
+  }
 
+  if (senha.value == "") {
+    senha.classList.add("errorInput");
+  } else {
+    senha.classList.remove("errorInput");
+  }
 
-        if(senha.value == ''){
-            senha.classList.add('errorInput');
-        } else {
-            senha.classList.remove('errorInput');
-        }
+  if (
+    email.value.indexOf("@") == -1 ||
+    email.value.indexOf(".") == -1 ||
+    email.value.indexOf(".") - email.value.indexOf("@") == 1
+  ) {
+    email.classList.add("errorInput");
+  } else {
+    email.classList.remove("errorInput");
+  }
 
-        if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 || (email.value.indexOf('.') - email.value.indexOf('@') == 1)) {
-            email.classList.add('errorInput');
-        } else {
-            email.classList.remove('errorInput');
-        }
+  if (senha.value.length <= 5) {
+    senha.classList.add("errorInput");
+  } else {
+    senha.classList.remove("errorInput");
+  }
 
-
-        if(senha.value.length <= 5) {
-            senha.classList.add('errorInput');
-        }else {
-            senha.classList.remove('errorInput');
-        }
-
-
-        if(senha.value.length >= 5 == true && email.value.indexOf('@','.') >= 0 == true) {
-            window.location.replace('./filmes.html');
-        }
-      
-      
-})
-
+  if ( senha.value.length >= 5 == true && email.value.indexOf("@", ".") >= 0 == true ) {
+    window.location.replace("./filmes.html");
+  }
+});
