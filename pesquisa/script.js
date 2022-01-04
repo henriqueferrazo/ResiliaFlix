2ccae1ff10af56458b4dcc40fae234592a74b616
Rodrigo Cruz <rodrigonascimentocruz@gmail.com>
2022-01-04 15:17:42 -0300

    Atualizado, ajustes finais API

--- pesquisa/script.js
+++ pesquisa/script.js
@@ -5,14 +5,15 @@ const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
 
-// load movies from API
+
 async function loadMovies(searchTerm){
     const URL = `https://omdbapi.com/?s=${searchTerm}&apikey=4b6071d7`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
-    // console.log(data.Search);
+   
     if(data.Response == "True") displayMovieList(data.Search);
 }
+
 
 function findMovies(){
     let searchTerm = (movieSearchBox.value).trim();
@@ -28,7 +29,7 @@ function displayMovieList(movies){
     searchList.innerHTML = "";
     for(let idx = 0; idx < movies.length; idx++){
         let movieListItem = document.createElement('div');
-        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
+        movieListItem.dataset.id = movies[idx].imdbID; 
         movieListItem.classList.add('search-list-item');
         if(movies[idx].Poster != "N/A")
             moviePoster = movies[idx].Poster;
@@ -53,10 +54,10 @@ function loadMovieDetails(){
     const searchListMovies = searchList.querySelectorAll('.search-list-item');
     searchListMovies.forEach(movie => {
         movie.addEventListener('click', async () => {
-            // console.log(movie.dataset.id);
+            
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=4b6071d7`);
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&page=1&apikey=4b6071d7`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
             displayMovieDetails(movieDetails);
